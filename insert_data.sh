6ac19101357a5b212b41094bf8bdd4dd9a551f83
allan-davincs <davincstechproject@gmail.com>
2026-05-07 03:09:54 -0400

    Save progress: 760.1

--- insert_data.sh
+++ insert_data.sh
@@ -2,8 +2,9 @@
 #Script to insert data from courses.csv and students.csv into students database
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 cat courses_test.csv | while IFS="," read MAJOR COURSE
-do 
-  do
+
+
+do
   if [[ $MAJOR != major ]]
   then
     # get major_id
